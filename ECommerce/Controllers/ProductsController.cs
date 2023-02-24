4c05bb4b6a034d1df99d8911fb9799eeb6e8d0c7
MZ <abdelazizmohamed985@gmail.com>
2023-02-24 18:25:36 +0200

    End Controller Part

---
+++ ECommerce/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ECommerceDbContext _context;
+        public ProductsController(ECommerceDbContext context)
+        {
+            _context = context;
+        }
+        public async Task <IActionResult> Index()
+        {
+            var Result = await _context.Categories.ToListAsync();
+            return View(Result);
+        }
+    }
+}
